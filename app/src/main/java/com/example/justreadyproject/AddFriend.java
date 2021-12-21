335cc2020640fd2172a44b5f9fe95c5d72c18895
João Carvalho <joaove>
2021-12-21 10:03:59 +0000

    c

--- app/src/main/java/com/example/justreadyproject/AddFriend.java
+++ app/src/main/java/com/example/justreadyproject/AddFriend.java
@@ -20,6 +20,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
@@ -28,12 +29,16 @@ public class AddFriend extends AppCompatActivity {
 
     EditText mTextAddFriend;
     Button mButtonAdd;
+    JSONArray usersId;
+    ArrayList<Integer> userId;
+    ArrayList<String> users;
     public static String USER_TICKETADD;
     public static String USER_IDFRIEND;
     public static String USER_NAMEID;
     public static String USER_RELNAMEID;
     public static String USER_GENDER;
     JSONObject Friend ;
+    public static int userId2;
     JSONArray Friendarr;
 
 
@@ -47,6 +52,31 @@ public class AddFriend extends AppCompatActivity {
         mTextAddFriend = (EditText) findViewById(R.id.addticket);
         mButtonAdd = (Button) findViewById(R.id.addfriendbtn);
 
+        JSONArrDownloader task1 = new JSONArrDownloader();
+    try {
+        usersId = task1.execute("https://justready.herokuapp.com/api/users").get();
+    } catch (ExecutionException e) {
+        e.printStackTrace();
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+
+    JSONObject obj;
+    users = new ArrayList<>();
+    userId = new ArrayList<>();
+        if(usersId != null) {
+        for (int i = 0; i < usersId.length();i++){
+            try{
+                obj = usersId.getJSONObject(i);
+                String userId1 = obj.getString("users_id");
+
+                userId.add(obj.getInt("users_id"));
+                users.add(String.format("%s",userId1));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 
 
@@ -58,16 +88,26 @@ public class AddFriend extends AppCompatActivity {
                 JSONArrDownloader getAddFriends = new JSONArrDownloader();
                 Intent i = new Intent(AddFriend.this,FriendsList.class);
                 USER_RELNAMEID = "1";
-                USER_IDFRIEND ="7";
+                USER_IDFRIEND ="31";
 
                 try {
 
                     USER_TICKETADD = mTextAddFriend.getText().toString();
 
                     Friendarr = getAddFriends.execute("https://justready.herokuapp.com/api/users/friendadd/"+ USER_TICKETADD).get();
+                    JSONObject obj;
+                    if (Friendarr != null) {
+                        for (int k = 0; k < Friendarr.length(); k++) {
+                            try {
+                                obj = Friendarr.getJSONObject(k);
+                                userId2 = Integer.parseInt(obj.getString("users_id"));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
 
-
-
+                    Log.e("USER ID", String.valueOf(userId2));
                     Log.e("Info Ticket",USER_TICKETADD);
 
                     try{
@@ -76,7 +116,7 @@ public class AddFriend extends AppCompatActivity {
                         postData.put("rl_rel_nameid",USER_RELNAMEID);
                         postData.put("rl_users_ticket",LoginJustReady.USER_TICKET);
                         postData.put("rl_users_idmain",LoginJustReady.USER_ID);
-                        postData.put("rl_users_idfriend",USER_IDFRIEND);
+                        postData.put("rl_users_idfriend", String.valueOf(userId2));
 
 
                         PostData task = new PostData(postData);
